039eeab3d94c794b202d2a48bafba6a33273aa2f
unmesh <unmeshjoshi@gmail.com>
2018-12-16 20:41:01 +0530

    added basic utilities to verify avro records

--- project/Libs.scala
+++ project/Libs.scala
@@ -1,6 +1,7 @@
 import sbt._
 import scalapb.compiler.Version.scalapbVersion
 
+//https://www.cloudera.com/documentation/enterprise/release-notes/topics/cdh_vd_cdh_package_tarball_514.html#cm_vd_cdh_package_tarball_514
 object CDHVersions {
   val Hbase = "1.2.0-cdh5.14.4"
   val Hadoop = "2.6.0-cdh5.14.4"
@@ -16,18 +17,13 @@ object Libs {
 
   val `scalatest` = "org.scalatest" %% "scalatest" % "3.0.4" //Apache License 2.0
   val `scala-java8-compat` = "org.scala-lang.modules" %% "scala-java8-compat" % "0.8.0" //BSD 3-clause "New" or "Revised" License
-  val `scala-async` = "org.scala-lang.modules" %% "scala-async" % "0.9.7" //BSD 3-clause "New" or "Revised" License
   val `scopt` = "com.github.scopt" %% "scopt" % "3.7.0" //MIT License
-  val `acyclic` = "com.lihaoyi" %% "acyclic" % "0.1.7" % Provided //MIT License
   val `junit` = "junit" % "junit" % "4.12" //Eclipse Public License 1.0
   val `junit-interface` = "com.novocode" % "junit-interface" % "0.11" //BSD 2-clause "Simplified" License
   val `mockito-core` = "org.mockito" % "mockito-core" % "2.12.0" //MIT License
   val `logback-classic` = "ch.qos.logback" % "logback-classic" % "1.2.3" //Dual license: Either, Eclipse Public License v1.0 or GNU Lesser General Public License version 2.1
-  val `akka-management-cluster-http` = "com.lightbend.akka" %% "akka-management-cluster-http" % "0.5" //N/A at the moment
   val svnkit = "org.tmatesoft.svnkit" % "svnkit" % "1.9.0" //TMate Open Source License
   val `commons-codec` = "commons-codec" % "commons-codec" % "1.10" //Apache 2.0
-  val `persist-json` = "com.persist" %% "persist-json" % "1.2.1" //Apache 2.0
-  val `joda-time` = "joda-time" % "joda-time" % "2.9.9" //Apache 2.0
   val `scala-reflect` = "org.scala-lang" % "scala-reflect" % ScalaVersion //BSD-3
   val `gson` = "com.google.code.gson" % "gson" % "2.8.2" //Apache 2.0
   val `play-json` = "com.typesafe.play" %% "play-json" % "2.6.7" //Apache 2.0
@@ -44,14 +40,14 @@ object Libs {
   val `geode` = "org.apache.geode" % "geode-core" % "1.6.0"
   val `hsqldb` = "org.hsqldb" % "hsqldb" % "1.8.0.10"  //2.4.1 gives connection error with no username/password
 
+  val `avro-mapred-hadoop2` = "org.apache.avro" % "avro-mapred" % "1.7.7" classifier "hadoop2"
+  val `scala-logging` = "com.typesafe.scala-logging" %% "scala-logging" % "3.7.2"
+  val `logback` = "ch.qos.logback" % "logback-classic" % "1.2.3"
+  val `logback-json-encoder` = "net.logstash.logback" % "logstash-logback-encoder" % "4.11" excludeAll ExclusionRule(
+    organization = "com.fasterxml.jackson.core"
+  )
 }
 
-object Jackson {
-  val Version = "2.9.2"
-  val `jackson-core` = "com.fasterxml.jackson.core" % "jackson-core" % Version
-  val `jackson-databind` = "com.fasterxml.jackson.core" % "jackson-databind" % Version
-  val `jackson-module-scala` = "com.fasterxml.jackson.module" %% "jackson-module-scala" % Version
-}
 
 object Enumeratum {
   val version = "1.5.12"
@@ -59,32 +55,6 @@ object Enumeratum {
   val `enumeratum-play` = "com.beachape" %% "enumeratum-play" % version //MIT License
 }
 
-object Chill {
-  val Version = "0.9.2"
-  val `chill-akka` = "com.twitter" %% "chill-akka" % Version //Apache License 2.0
-  val `chill-bijection` = "com.twitter" %% "chill-bijection" % Version //Apache License 2.0
-}
-
-object Akka {
-  val Version = "2.5.10" //all akka is Apache License 2.0
-  val `akka-stream` = "com.typesafe.akka" %% "akka-stream" % Version
-  val `akka-remote` = "com.typesafe.akka" %% "akka-remote" % Version
-  val `akka-stream-testkit` = "com.typesafe.akka" %% "akka-stream-testkit" % Version
-  val `akka-actor` = "com.typesafe.akka" %% "akka-actor" % Version
-  val `akka-typed` = "com.typesafe.akka" %% "akka-typed" % Version
-  val `akka-typed-testkit` = "com.typesafe.akka" %% "akka-typed-testkit" % Version
-  val `akka-distributed-data` = "com.typesafe.akka" %% "akka-distributed-data" % Version
-  val `akka-multi-node-testkit` = "com.typesafe.akka" %% "akka-multi-node-testkit" % Version
-  val `akka-cluster-tools` = "com.typesafe.akka" %% "akka-cluster-tools" % Version
-  val `akka-slf4j` = "com.typesafe.akka" %% "akka-slf4j" % Version
-}
-
-object AkkaHttp {
-  val Version = "10.0.10"
-  val `akka-http` = "com.typesafe.akka" %% "akka-http" % Version //ApacheV2
-  val `akka-http-testkit` = "com.typesafe.akka" %% "akka-http-testkit" % Version //ApacheV2
-  val `akka-http2` = "com.typesafe.akka" %% "akka-http2-support" % Version
-}
 
 object Kafka {
   val `kafkaStreamsScala` = "com.lightbend" %% "kafka-streams-scala" % "0.1.0"
@@ -133,6 +103,7 @@ object Spark {
   val `spark-sql-test` = "org.apache.spark" %% "spark-sql" % CDHVersions.Spark % "test" classifier "tests"  excludeAll(
     ExclusionRule(organization = "com.google.guava", "guava"))
 
+  val `spark-avro` = "com.databricks" %% "spark-avro" % "4.0.0"
 
 }
 
@@ -170,20 +141,6 @@ object Yaml {
 object JSON {
   val JsonVersion = "20171018"
   val `json` = "org.json" % "json" % JsonVersion
-}
-
-object OtherJars {
-
-  val `commons-lang3` = "org.apache.commons" % "commons-lang3" % "3.0"
-  val `guava` = "com.google.guava" % "guava" % "15.0"
-  val `httpcore` = "org.apache.httpcomponents" % "httpcore" % "4.2.2"
-  val `spark-avro` = "com.databricks" %% "spark-avro" % "4.0.0"
-  val `avro-mapred-hadoop2` = "org.apache.avro" % "avro-mapred" % "1.7.7" classifier "hadoop2"
-  val `scala-logging` = "com.typesafe.scala-logging" %% "scala-logging" % "3.7.2"
-  val `logback` = "ch.qos.logback" % "logback-classic" % "1.2.3"
-  val `logback-json-encoder` = "net.logstash.logback" % "logstash-logback-encoder" % "4.11" excludeAll ExclusionRule(
-    organization = "com.fasterxml.jackson.core"
-  )
 }
 
 object Excluded {
--- src/test/scala/com/hbaseservices/sqoop/SqoopImportTestBase.scala
+++ src/test/scala/com/hbaseservices/sqoop/SqoopImportTestBase.scala
@@ -1,110 +1,114 @@
 package com.hbaseservices.sqoop
 
-import java.io.File
+import java.io.{File, FilenameFilter}
 import java.sql.{Connection, DriverManager, SQLException}
-import java.util
 
-import com.cloudera.sqoop.SqoopOptions
-import com.cloudera.sqoop.SqoopOptions.FileLayout
+import org.apache.avro.file.DataFileReader
+import org.apache.avro.generic.{GenericDatumReader, GenericRecord}
+import org.apache.avro.mapred.FsInput
 import org.apache.commons.io.FileUtils
-import org.apache.hadoop.fs.FileUtil
-import org.apache.hadoop.mapred.JobConf
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
 import org.hsqldb.Server
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
 
 class SqoopImportTestBase extends FunSuite with BeforeAndAfterAll {
-  val TARGET_DIR = "/tmp/hadoop"
-  private val IN_MEM = "mem:"
-  private val tmpDir: String = System.getProperty("test.build.data", "/tmp/")
-  private val EMPLOYEE_TABLE_NAME = "EMPLOYEES"
-  private val DB_URL = "jdbc:hsqldb:" + getServerHost + DATABASE_NAME
-  private val DATABASE_NAME = System.getProperty("hsql.database.name", "db1")
-  private val DRIVER_CLASS = "org.hsqldb.jdbcDriver"
-  var dbLocation: String = tmpDir + "/sqoop/testdb.file"
-
-  def getServerHost: String = {
-    var host = System.getProperty("hsql.server.host", IN_MEM)
-    if (!host.endsWith("/")) host += "/"
-    host
-  }
-
-  val server = new Server
+  val testHadoop = new TestHadoop(tempDir)
+  val testDatabase = new TestDatabase(tempDir)
+  private val tempDir = "/tmp"
 
   override def beforeAll(): Unit = {
-    startDatabase()
-    createTestData()
-  }
-
-  private def startDatabase ()= {
-    server.setDatabaseName(0, "test")
-    server.putPropertiesFromString("database.0=" + dbLocation + ";no_system_exit=true")
-    server.start
+    testDatabase.start()
   }
 
   override def afterAll(): Unit = {
-    server.stop
-    FileUtils.deleteDirectory(new File(TARGET_DIR))
+    testDatabase.stop()
+    testHadoop.deleteAll()
   }
 
-  def createTestData() = {
-    //TODO: Figure out if we need to set username and password explicitly.
-    //    executeUpdate(String.format("CREATE USER %s PASSWORD %s ADMIN", "testuser", "testpassword"))
-    executeUpdate("CREATE TABLE " + EMPLOYEE_TABLE_NAME + "(name VARCHAR(50));")
-    executeUpdate("INSERT INTO " + EMPLOYEE_TABLE_NAME + " VALUES ('FirstName')")
-    executeUpdate("INSERT INTO " + EMPLOYEE_TABLE_NAME + " VALUES ('FirstName1')")
-    executeUpdate("INSERT INTO " + EMPLOYEE_TABLE_NAME + " VALUES ('FirstName2')")
+  def verifyAvroOutputFile(targetDir: String, names: List[String], valueList: List[List[String]]): Unit = {
+    val files = new File(targetDir).listFiles(new FilenameFilter {
+      override def accept(dir: File, name: String): Boolean = name.endsWith(".avro")
+    })
+    assert(files.size == 1)
+    val reader = read(new Path(files(0).getAbsolutePath), new Configuration())
+    valueList.foreach(values ⇒ {
+      val record = reader.next()
+      validateRecord(record, names, values)
+    }
+    )
   }
 
-  private def executeUpdate(query: String) = {
-    val connection = getConnection("", "")
-    val statement = connection.createStatement
-    try {
-      try
-        statement.executeUpdate(query)
+  def validateRecord(record: GenericRecord, names: List[String], values: List[String]) = {
+    val tuples = names.zip(values)
+    tuples.foreach(tuple ⇒ {
+      assert(record.get(tuple._1.toUpperCase()).toString == tuple._2)
+     })
+
+  }
+
+  def read(filename: Path, conf: Configuration): DataFileReader[GenericRecord] = {
+    conf.set("fs.defaultfs.name", "fs.defaultfs.name")
+    val fsInput = new FsInput(filename, conf)
+    val datumReader = new GenericDatumReader[GenericRecord]
+    new DataFileReader[GenericRecord](fsInput, datumReader)
+  }
+
+  class TestHadoop(val baseDir: String) {
+    val baseHadoopDir = s"${baseDir}/hadoop"
+
+    def targetDirFor(tableName: String) = {
+      s"${baseHadoopDir}/${tableName}"
+    }
+
+    def deleteAll() = {
+      FileUtils.deleteDirectory(new File(baseHadoopDir))
+    }
+  }
+
+  class TestDatabase(val baseDir: String) {
+    private val DB_URL = "jdbc:hsqldb:mem:db1"
+    private val DRIVER_CLASS = "org.hsqldb.jdbcDriver"
+    private val dbLocation: String = s"${baseDir}/sqoop/testdb.file"
+    private val server = new Server
+
+    def start() = {
+      server.setDatabaseName(0, "test")
+      server.putPropertiesFromString("database.0=" + dbLocation + ";no_system_exit=true")
+      server.start
+    }
+
+    def executeUpdate(queries: List[String]) = {
+      val connection = getConnection("", "")
+      val statement = connection.createStatement
+      try {
+        queries.foreach(query ⇒ {
+          statement.executeUpdate(query)
+        })
         connection.commit()
-    } finally {
-      if (connection != null) connection.close()
-      if (statement != null) statement.close()
+      } finally {
+        if (connection != null) connection.close()
+        if (statement != null) statement.close()
+      }
     }
 
-  }
-
-  @throws[SQLException]
-  def getConnection(user: String, password: String): Connection = {
-    try
-      Class.forName(DRIVER_CLASS)
-    catch {
-      case cnfe: ClassNotFoundException ⇒
-        println("Could not get connection; driver class not found: " + DRIVER_CLASS)
-        return null
+    @throws[SQLException]
+    private def getConnection(user: String, password: String): Connection = {
+      try
+        Class.forName(DRIVER_CLASS)
+      catch {
+        case cnfe: ClassNotFoundException ⇒
+          println("Could not get connection; driver class not found: " + DRIVER_CLASS)
+          return null
+      }
+      val connection = DriverManager.getConnection(getJdbcConnectUrl())
+      connection.setAutoCommit(false)
+      connection
     }
-    val connection = DriverManager.getConnection(DB_URL)
-    connection.setAutoCommit(false)
-    connection
+
+    def getJdbcConnectUrl() = DB_URL
+
+    def stop() = server.stop()
   }
 
-  def buildSqoopOptions() = {
-    val conf = new JobConf() //TODO: Figure out if we need conf to be set explicitly
-    //    conf.set("mapreduce.job.queuename", "testqueue")
-    //    conf.set("mapreduce.jobtracker.address", "local")
-    //    conf.set("mapreduce.job.maps", "1")
-    //    conf.set("fs.defaultFS", "file:///")
-    //    conf.set("jobclient.completion.poll.interval", "50")
-    //    conf.set("jobclient.progress.monitor.poll.interval", "50")
-    //    conf.set("mapreduce.jdbc.driver.class", DRIVER_CLASS)
-
-    val options = new SqoopOptions(conf)
-    options.setConnectString(DB_URL) //jdbc
-    options.setSqlQuery("select * from " + EMPLOYEE_TABLE_NAME)
-    options.setSplitByCol("name")
-    options.setNumMappers(1)
-    options.setTargetDir(TARGET_DIR)
-    options.setFileLayout(FileLayout.AvroDataFile)
-    options.setDeleteMode(false)
-    options.setAppendMode(true)
-    options.setClassName("test")
-    options.setTableName(EMPLOYEE_TABLE_NAME)
-    options.setCustomToolOptions(new util.HashMap[String, String]()) //need to set or else get NPE
-    options
-  }
 }
