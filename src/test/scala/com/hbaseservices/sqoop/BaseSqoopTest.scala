23266481298b75b33c08fd803e970ebc12e2116a
unmesh <unmeshjoshi@gmail.com>
2018-12-16 20:45:32 +0530

    added basic utilities to verify avro records

---
+++ src/test/scala/com/hbaseservices/sqoop/BaseSqoopTest.scala
@@ -0,0 +1,114 @@
+package com.hbaseservices.sqoop
+
+import java.io.{File, FilenameFilter}
+import java.sql.{Connection, DriverManager, SQLException}
+
+import org.apache.avro.file.DataFileReader
+import org.apache.avro.generic.{GenericDatumReader, GenericRecord}
+import org.apache.avro.mapred.FsInput
+import org.apache.commons.io.FileUtils
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
+import org.hsqldb.Server
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
+
+abstract class BaseSqoopTest extends FunSuite with BeforeAndAfterAll {
+  private val tempDir = "/tmp"
+  val testHadoop = new TestHadoop(tempDir)
+  val testDatabase = new TestDatabase(tempDir)
+
+  override def beforeAll(): Unit = {
+    testDatabase.start()
+  }
+
+  override def afterAll(): Unit = {
+    testDatabase.stop()
+    testHadoop.deleteAll()
+  }
+
+  def verifyAvroOutputFile(targetDir: String, names: List[String], valueList: List[List[String]]): Unit = {
+    val files = new File(targetDir).listFiles(new FilenameFilter {
+      override def accept(dir: File, name: String): Boolean = name.endsWith(".avro")
+    })
+    assert(files.size == 1)
+    val reader = read(new Path(files(0).getAbsolutePath), new Configuration())
+    valueList.foreach(values ⇒ {
+      val record = reader.next()
+      validateRecord(record, names, values)
+    }
+    )
+  }
+
+  def validateRecord(record: GenericRecord, names: List[String], values: List[String]) = {
+    val tuples = names.zip(values)
+    tuples.foreach(tuple ⇒ {
+      assert(record.get(tuple._1.toUpperCase()).toString == tuple._2)
+     })
+
+  }
+
+  def read(filename: Path, conf: Configuration): DataFileReader[GenericRecord] = {
+    conf.set("fs.defaultfs.name", "fs.defaultfs.name")
+    val fsInput = new FsInput(filename, conf)
+    val datumReader = new GenericDatumReader[GenericRecord]
+    new DataFileReader[GenericRecord](fsInput, datumReader)
+  }
+
+  class TestHadoop(val baseDir: String) {
+    val baseHadoopDir = s"${baseDir}/hadoop"
+
+    def targetDirFor(tableName: String) = {
+      s"${baseHadoopDir}/${tableName}"
+    }
+
+    def deleteAll() = {
+      FileUtils.deleteDirectory(new File(baseHadoopDir))
+    }
+  }
+
+  class TestDatabase(val baseDir: String) {
+    private val DB_URL = "jdbc:hsqldb:mem:db1"
+    private val DRIVER_CLASS = "org.hsqldb.jdbcDriver"
+    private val dbLocation: String = s"${baseDir}/sqoop/testdb.file"
+    private val server = new Server
+
+    def start() = {
+      server.setDatabaseName(0, "test")
+      server.putPropertiesFromString("database.0=" + dbLocation + ";no_system_exit=true")
+      server.start
+    }
+
+    def executeUpdate(queries: List[String]) = {
+      val connection = getConnection("", "")
+      val statement = connection.createStatement
+      try {
+        queries.foreach(query ⇒ {
+          statement.executeUpdate(query)
+        })
+        connection.commit()
+      } finally {
+        if (connection != null) connection.close()
+        if (statement != null) statement.close()
+      }
+    }
+
+    @throws[SQLException]
+    private def getConnection(user: String, password: String): Connection = {
+      try
+        Class.forName(DRIVER_CLASS)
+      catch {
+        case cnfe: ClassNotFoundException ⇒
+          println("Could not get connection; driver class not found: " + DRIVER_CLASS)
+          return null
+      }
+      val connection = DriverManager.getConnection(getJdbcConnectUrl())
+      connection.setAutoCommit(false)
+      connection
+    }
+
+    def getJdbcConnectUrl() = DB_URL
+
+    def stop() = server.stop()
+  }
+
+}
--- src/test/scala/com/hbaseservices/sqoop/SqoopImportTest.scala
+++ src/test/scala/com/hbaseservices/sqoop/SqoopImportTest.scala
@@ -1,114 +1,57 @@
 package com.hbaseservices.sqoop
 
-import java.io.{File, FilenameFilter}
-import java.sql.{Connection, DriverManager, SQLException}
+import java.util
 
-import org.apache.avro.file.DataFileReader
-import org.apache.avro.generic.{GenericDatumReader, GenericRecord}
-import org.apache.avro.mapred.FsInput
-import org.apache.commons.io.FileUtils
-import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.Path
-import org.hsqldb.Server
-import org.scalatest.{BeforeAndAfterAll, FunSuite}
+import com.cloudera.sqoop.SqoopOptions
+import com.cloudera.sqoop.SqoopOptions.FileLayout
+import org.apache.hadoop.mapred.JobConf
 
-abstract class SqoopImportTest extends FunSuite with BeforeAndAfterAll {
-  private val tempDir = "/tmp"
-  val testHadoop = new TestHadoop(tempDir)
-  val testDatabase = new TestDatabase(tempDir)
+class SqoopImportTest extends BaseSqoopTest {
 
-  override def beforeAll(): Unit = {
-    testDatabase.start()
+  test("should import from database to hadoop") {
+    createTestData()
+
+    new DataImporter().importData(buildSqoopOptions("employees",
+      "select * from employees where name='FirstName2' AND $CONDITIONS",
+      "name"))
+
+    val targetDir = testHadoop.targetDirFor("employees")
+    verifyAvroOutputFile(targetDir, List("name"), List(List("FirstName2")))
   }
 
-  override def afterAll(): Unit = {
-    testDatabase.stop()
-    testHadoop.deleteAll()
+  def createTestData() = {
+    //TODO: Figure out if we need to set username and password explicitly.
+    //    executeUpdate(String.format("CREATE USER %s PASSWORD %s ADMIN", "testuser", "testpassword"))
+    val queries = List("CREATE TABLE employees (name varchar(50));",
+      "INSERT INTO employees VALUES ('FirstName')",
+      "INSERT INTO employees VALUES ('FirstName1')",
+      "INSERT INTO employees VALUES ('FirstName2')")
+    
+    testDatabase.executeUpdate(queries)
   }
 
-  def verifyAvroOutputFile(targetDir: String, names: List[String], valueList: List[List[String]]): Unit = {
-    val files = new File(targetDir).listFiles(new FilenameFilter {
-      override def accept(dir: File, name: String): Boolean = name.endsWith(".avro")
-    })
-    assert(files.size == 1)
-    val reader = read(new Path(files(0).getAbsolutePath), new Configuration())
-    valueList.foreach(values ⇒ {
-      val record = reader.next()
-      validateRecord(record, names, values)
-    }
-    )
+  def buildSqoopOptions(tableName: String, sqlQuery: String, splitByColumn: String): SqoopOptions = {
+    val conf = new JobConf()
+    //TODO: Figure out if we need conf to be set explicitly
+    //    conf.set("mapreduce.job.queuename", "testqueue")
+    //    conf.set("mapreduce.jobtracker.address", "local")
+    //    conf.set("mapreduce.job.maps", "1")
+    //    conf.set("fs.defaultFS", "file:///")
+    //    conf.set("jobclient.completion.poll.interval", "50")
+    //    conf.set("jobclient.progress.monitor.poll.interval", "50")
+    //    conf.set("mapreduce.jdbc.driver.class", DRIVER_CLASS)
+
+    val options = new SqoopOptions(conf)
+    options.setConnectString(testDatabase.getJdbcConnectUrl()) //jdbc
+    options.setSqlQuery(sqlQuery)
+    options.setSplitByCol(splitByColumn)
+    options.setNumMappers(1)
+    options.setTargetDir(testHadoop.targetDirFor(tableName))
+    options.setFileLayout(FileLayout.AvroDataFile)
+    options.setDeleteMode(false)
+    options.setAppendMode(true)
+    options.setClassName("test")
+    options.setCustomToolOptions(new util.HashMap[String, String]()) //need to set or else get NPE
+    options
   }
-
-  def validateRecord(record: GenericRecord, names: List[String], values: List[String]) = {
-    val tuples = names.zip(values)
-    tuples.foreach(tuple ⇒ {
-      assert(record.get(tuple._1.toUpperCase()).toString == tuple._2)
-     })
-
-  }
-
-  def read(filename: Path, conf: Configuration): DataFileReader[GenericRecord] = {
-    conf.set("fs.defaultfs.name", "fs.defaultfs.name")
-    val fsInput = new FsInput(filename, conf)
-    val datumReader = new GenericDatumReader[GenericRecord]
-    new DataFileReader[GenericRecord](fsInput, datumReader)
-  }
-
-  class TestHadoop(val baseDir: String) {
-    val baseHadoopDir = s"${baseDir}/hadoop"
-
-    def targetDirFor(tableName: String) = {
-      s"${baseHadoopDir}/${tableName}"
-    }
-
-    def deleteAll() = {
-      FileUtils.deleteDirectory(new File(baseHadoopDir))
-    }
-  }
-
-  class TestDatabase(val baseDir: String) {
-    private val DB_URL = "jdbc:hsqldb:mem:db1"
-    private val DRIVER_CLASS = "org.hsqldb.jdbcDriver"
-    private val dbLocation: String = s"${baseDir}/sqoop/testdb.file"
-    private val server = new Server
-
-    def start() = {
-      server.setDatabaseName(0, "test")
-      server.putPropertiesFromString("database.0=" + dbLocation + ";no_system_exit=true")
-      server.start
-    }
-
-    def executeUpdate(queries: List[String]) = {
-      val connection = getConnection("", "")
-      val statement = connection.createStatement
-      try {
-        queries.foreach(query ⇒ {
-          statement.executeUpdate(query)
-        })
-        connection.commit()
-      } finally {
-        if (connection != null) connection.close()
-        if (statement != null) statement.close()
-      }
-    }
-
-    @throws[SQLException]
-    private def getConnection(user: String, password: String): Connection = {
-      try
-        Class.forName(DRIVER_CLASS)
-      catch {
-        case cnfe: ClassNotFoundException ⇒
-          println("Could not get connection; driver class not found: " + DRIVER_CLASS)
-          return null
-      }
-      val connection = DriverManager.getConnection(getJdbcConnectUrl())
-      connection.setAutoCommit(false)
-      connection
-    }
-
-    def getJdbcConnectUrl() = DB_URL
-
-    def stop() = server.stop()
-  }
-
 }
